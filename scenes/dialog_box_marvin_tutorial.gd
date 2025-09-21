499bdb16a1973b4938575d8905e9730c343b34da
Keila Queiroz Cortes <kqueiroz@c2r8p2.42sp.org.br>
2025-09-21 05:17:08 -0300

    add tutorial1 e 2 marvin

--- scenes/dialog_box_marvin_tutorial.gd
+++ scenes/dialog_box_marvin_tutorial.gd
@@ -1,38 +1,48 @@
 extends NinePatchRect
 
-@onready var text = $text as RichTextLabel
+@onready var text = $RichTextLabel
 @onready var timer = $Timer
 
 var msg_queue: Array = [
-	"Olá",
-	"Diálogo Marvin"
+	" " +
+	" " +
+	"De novo.",
+	"Sim, sua vida depende de pressionar teclas corretamente. " +
+	"Estou tão entusiasmado quanto você imagina... menos, talvez.",
+	"Olhe, humano. Se você não digitar os comandos corretos, " +
+	"os monstros vão continuar destruindo seus projetos. " +
+	"Nada pessoal."
 ]
-var current_message_length: int = 0
+
+var current_message: String = ""
+var is_typing: bool = false
 
 func _input(event):
 	if event is InputEventKey and event.is_action_pressed("ui_accept"):
-		show_message()
+		if is_typing:
+			# Completa a mensagem atual
+			text.visible_characters = -1  # Mostra todo o texto
+			timer.stop()
+			is_typing = false
+		else:
+			# Avança para próxima mensagem
+			show_next_message()
 
-func show_message() -> void:
-	if not timer.is_stopped():
-		text.visible_characters = current_message_length
-		return
+func show_next_message():
 	if msg_queue.size() == 0:
 		hide()
 		return
 	
-	var _msg: String = msg_queue.pop_front()
-	
+	current_message = msg_queue.pop_front()
+	text.text = current_message
 	text.visible_characters = 0
-	text.text = _msg
-	# Força a atualização do texto para calcular o comprimento correto
-	await get_tree().process_frame
-	current_message_length = text.get_total_character_count()
-	timer.start()
+	is_typing = true
+	
+	timer.start(0.05)
 
 func _on_timer_timeout():
-	if text.visible_characters >= current_message_length:
+	if text.visible_characters < text.get_total_character_count():
+		text.visible_characters += 1
+	else:
 		timer.stop()
-		return
-	
-	text.visible_characters += 1
+		is_typing = false
